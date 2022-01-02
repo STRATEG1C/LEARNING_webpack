e8e56186e2ef8ffea37c29f5fccbedac5f44fb88
STRATEG1C <strateg4k@gmail.com>
2022-01-02 19:18:03 +0200

    Add auto generating html file in build. Split bundles. Add dist folder to gitignore

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './assets/icon.png';
-import Data from './data/data.xml';
-import Notes from './data/data.csv';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  myIcon.width = 100;
-  myIcon.height = 100;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
+  element.appendChild(btn);
 
   return element;
 }
